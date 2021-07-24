2176724b0c0b02972b436c4de481c2a92e42b00c
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2021-07-24 22:10:12 +0100

    update clang-format rules

--- main-scroll-macos.cpp
+++ main-scroll-macos.cpp
@@ -28,8 +28,7 @@ int main(int argc, char** argv) {
   //   collapser.collapse(handle, entities);
   // }
 
-  hy::view_t view(
-    hy::flatten_entities(entities, collapser, root_handles), 0, 20);
+  hy::view_t view(hy::flatten_entities(entities, collapser, root_handles), 0, 20);
 
   for (bool running = true; running;) {
     clear();
@@ -38,29 +37,22 @@ int main(int argc, char** argv) {
     // find another matching indent before a lower indent is found
     std::vector<bool> ends;
     for (int indent_index = 0;
-         indent_index
-         < std::min((int)view.flattened_handles().size(), view.count());
+         indent_index < std::min((int)view.flattened_handles().size(), view.count());
          ++indent_index) {
       int next_indent_index = indent_index + 1;
       if (auto found = std::find_if(
-            view.flattened_handles().begin() + view.offset()
-              + next_indent_index,
+            view.flattened_handles().begin() + view.offset() + next_indent_index,
             view.flattened_handles().end(),
             [&view, indent_index](const auto& flattened_handle) {
-              return view.flattened_handles()[indent_index + view.offset()]
-                       .indent_
+              return view.flattened_handles()[indent_index + view.offset()].indent_
                   == flattened_handle.indent_;
             });
           found != view.flattened_handles().end()) {
-        int range = found
-                  - (view.flattened_handles().begin() + view.offset()
-                     + next_indent_index);
+        int range = found - (view.flattened_handles().begin() + view.offset() + next_indent_index);
         bool end = false;
         for (int i = 0; i < range; i++) {
           int is = next_indent_index + i;
-          if (
-            view.flattened_handles()[is + view.offset()].indent_
-            < found->indent_) {
+          if (view.flattened_handles()[is + view.offset()].indent_ < found->indent_) {
             end = true;
             ends.push_back(true);
             break;
@@ -69,15 +61,12 @@ int main(int argc, char** argv) {
         if (!end) {
           for (int i = 0; i < range; i++) {
             int is = next_indent_index + i;
-            int min = std::min(
-              view.count(),
-              (int)view.flattened_handles().size() - view.offset());
+            int min = std::min(view.count(), (int)view.flattened_handles().size() - view.offset());
             if (is >= min) {
               break;
             }
             connections.push_back(
-              {view.flattened_handles()[indent_index + view.offset()].indent_,
-               is});
+              {view.flattened_handles()[indent_index + view.offset()].indent_, is});
           }
           ends.push_back(false);
         }
@@ -86,25 +75,20 @@ int main(int argc, char** argv) {
       }
     }
 
-    assert(
-      ends.size()
-      == std::min((int)view.flattened_handles().size(), view.count()));
+    assert(ends.size() == std::min((int)view.flattened_handles().size(), view.count()));
 
     for (const auto& connection : connections) {
       mvprintw(connection.second, connection.first * 4, "\xE2\x94\x82");
     }
 
-    const int count = std::min(
-      (int)view.flattened_handles().size(), view.offset() + view.count());
-    for (int handle_index = view.offset(); handle_index < count;
-         ++handle_index) {
+    const int count = std::min((int)view.flattened_handles().size(), view.offset() + view.count());
+    for (int handle_index = view.offset(); handle_index < count; ++handle_index) {
       const auto& flattened_handle = view.flattened_handles()[handle_index];
 
       mvprintw(
         handle_index - view.offset(), flattened_handle.indent_ * 4,
-        ends[handle_index - view.offset()]
-          ? "\xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 "
-          : "\xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 ");
+        ends[handle_index - view.offset()] ? "\xE2\x94\x94\xE2\x94\x80\xE2\x94\x80 "
+                                           : "\xE2\x94\x9C\xE2\x94\x80\xE2\x94\x80 ");
 
       if (handle_index == view.selected()) {
         attron(A_REVERSE);
